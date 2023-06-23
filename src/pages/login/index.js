8e682c36d0cb36edde110facaf15ec55f3fdc18b
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-06-23 15:56:26 +0530

    Add login page (#5)
    * Added a login page background image
    
    * Added global color
    
    * Added image in login page
    
    * Added title
    
    * Added icons
    
    * Added O-Auth provider login buttons
    
    * Updated github icon when theme changes

---
+++ src/pages/login/index.js
@@ -0,0 +1,77 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/login.module.css'
+import Typewriter from "typewriter-effect";
+import {useEffect, useState} from "react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function Login() {
+    const [isDarkTheme, setDarkTheme] = useState(false)
+    const [change, setChange] = useState(false)
+    setInterval(()=>{setChange(!change)}, 1000)
+
+    useEffect(()=>{
+        setDarkTheme(window.matchMedia("(prefers-color-scheme: dark)").matches)
+    }, [change])
+    return (
+        <>
+            <Head>
+                <title>Login | HRV Mart Admin Portal</title>
+                <meta name="description" content="HRV-Mart admin portal" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={`${inter.className} ${styles.main}`}>
+                <div className={styles.leftContainer}/>
+                <hr className={styles.verticalDivider}/>
+                <div className={styles.rightContainer}>
+                    <div className={styles.title}>
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    "Welcome back to HRV-Mart Admin portal",
+                                    "Please login with your account"
+                                ],
+                                autoStart: true,
+                                loop: true
+                            }}
+                        />
+                    </div>
+                    <div className={styles.loginContainer}>
+                        <div onClick={()=>oauth_2_handler('github')} className={styles.loginButton}>
+                            <div className={styles.brand_icon_container}>
+                                <img src={isDarkTheme ? '/github_icon_white.svg':'/github_icon.svg'} className={`${styles.brand_icon}`}/>
+                            </div>
+                            <div className={styles.loginTitle}>
+                                Login with GitHub
+                            </div>
+                        </div>
+
+                        <div onClick={()=>oauth_2_handler('discord')} className={styles.loginButton}>
+                            <div className={styles.brand_icon_container}>
+                                <img src={'/discord_icon.svg'} className={`${styles.brand_icon}`}/>
+                            </div>
+                            <div className={styles.loginTitle}>
+                                Login with Discord
+                            </div>
+                        </div>
+
+                        <div onClick={()=>oauth_2_handler('google')} className={styles.loginButton}>
+                            <div className={styles.brand_icon_container}>
+                                <img src={'/google_icon.svg'} className={`${styles.brand_icon}`}/>
+                            </div>
+                            <div className={styles.loginTitle}>
+                                Login with Google
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+    function oauth_2_handler(provider_name) {
+
+    }
+}
