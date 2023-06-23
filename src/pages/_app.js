2200e993c25c59607e81e17935836f0b9a027309
Harsh3305 <harshverma3305@gmail.com>
2023-06-23 16:28:04 +0530

    Added footer in app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import Header from "@/component/header";
+import Footer from "@/component/footer";
 
 export default function App({ Component, pageProps }) {
   return <>
     <Header/>
     <Component {...pageProps} />
+    <Footer/>
   </>
 }
