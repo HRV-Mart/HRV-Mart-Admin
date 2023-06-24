476f34cff76a23e27bfb6bd76cf02f83c63702c9
Harsh3305 <harshverma3305@gmail.com>
2023-06-24 20:35:02 +0530

    Access appwrite config from env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,10 @@ import Header from "@/component/header";
 import Footer from "@/component/footer";
 
 export default function App({ Component, pageProps }) {
+  const appwrite_endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+  const appwrite_project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+  const application_url = process.env.NEXT_PUBLIC_APPLICATION_URL;
+
   return <>
     <Header/>
     <Component {...pageProps} />
