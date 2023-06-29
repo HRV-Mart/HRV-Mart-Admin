5947cff6a12d162d0bf8f8424a9e6971dba2823b
Harsh3305 <harshverma3305@gmail.com>
2023-06-29 14:20:36 +0530

    Updated _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,11 @@ import '@/styles/globals.css'
 import Header from "@/component/header";
 import Footer from "@/component/footer";
 import {useEffect, useState} from "react";
-import {logError} from "@/service/logging/logging";
+import {logError, logMessage} from "@/service/logging/logging";
 import {Account, Client} from "appwrite";
 import {postRequest} from "@/service/network/network";
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from "react-toastify";
 
 export default function App({ Component, pageProps }) {
   const appwrite_endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
@@ -15,6 +17,7 @@ export default function App({ Component, pageProps }) {
     account: null,
     token: null
   })
+  logMessage(details)
 
   useEffect(()=>{
     generateToken().then().catch(logError);
@@ -25,6 +28,7 @@ export default function App({ Component, pageProps }) {
     <Header token={details.token} />
     <Component {...pageProps} token={details.token} account={details.account} application_url={application_url} />
     <Footer />
+    <ToastContainer />
   </>
 
   async function generateToken() {
@@ -48,15 +52,14 @@ export default function App({ Component, pageProps }) {
           {
             "Content-Type": "application/json"
           },
-          true
+          false
       )
 
-      token = result.data.token
+      token = result.data
     }
     catch (error) {
       logError(error)
     }
-
     setDetails({
       account: account,
       token: token
